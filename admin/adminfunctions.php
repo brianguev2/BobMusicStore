213b30c7b44d195f272571493817110e8072fde0
Brian Guevara <brianguev2@gmail.com>
2018-01-06 01:00:40 -0500

    Add files via upload

---
+++ admin/adminfunctions.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<!--Displays admin functions.-->
+<?php include 'adminheader.php'; ?>
+
+      <h1>Select an option below: </h1><br>
+      <h4>Welcome, Admin!</h4><br>
+      <p><a href="createaccount.php">Create Admin Account</a></p>
+      <p><a href="employeecreateaccount.php">Create Employee Account</a></p>
+      <p><a href="createmanager.php">Create Manager Account</a></p>
+      <p><a href="accountlist.php">View Customer List</a></p>
+      <p><a href="employeelist.php">View Employee List</a></p>
+      <p><a href="managerlist.php">View Manager List</a></p>
+      <p><a href="adminlist.php">View Admin List</a></p>  
+      <?php include '../footer.php'; ?>
+  </body>
+
+</html>
---
+++ manager/managerindex.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<!--Displays page upon successful login.-->
+<?php include 'managerheader.php'; ?>
+<body>
+      <p>Select an option below:</p>
+      <p><a href="employeecreateaccount.php">Create Employee Account</a></p>
+      <p><a href="employeelist.php">View Employee List</a></p>
+      <p><a href="productlist.php">View Product List</a></p>
+      <p><a href="addproduct.php">Add Product</a></p>
+    
+      <?php include '../footer.php'; ?>
+  </body>
+
+</html>
